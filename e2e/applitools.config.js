e66d8f1b11f50562440902fb0df558cff3ed59a7
Dieme-Musabyimana <musabyimanadieme10@gmail.com>
2026-03-04 18:24:41 +0200

    add space

--- e2e/applitools.config.js
+++ e2e/applitools.config.js
@@ -1,4 +1,10 @@
+//module.exports = {
+//    apiKey: f98X1Y71kWlje2ekG7y2ZYqLOoB78Yz798VRCWfwJZ80g110,
+//    appName:'Automation Bookstore'
+//}
 module.exports = {
     apiKey: process.env.APPLITOOLS_API_KEY,
-    appName:'Automation Bookstore'
+    appName: 'Automation Bookstore',
+    batchName: 'BDD Visual Tests',
+    browser: { width: 800, height: 600, name: 'chrome' }
 }
